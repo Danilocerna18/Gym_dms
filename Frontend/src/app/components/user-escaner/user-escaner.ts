7142be1806b969d3d04ba272020d90bdb6096c14
Miguel Cuevas <miguel.cuevas@ufm.edu>
2026-09-06 11:22:57 -0600

    escaner de maquinas

---
+++ Frontend/src/app/components/user-escaner/user-escaner.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-cliente-perfil',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cliente-perfil.component.html',
+  styleUrls: ['./cliente-perfil.component.css']
+})
+export class ClientePerfilComponent {
+
+}
