0dbf9334aec7369b050e34d58ff5d7c2a2745049
Danil38 <danilo.cerna@ufm.edu>
2026-09-05 23:08:20 -0600

    dejandolo como sofia

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { AdminPanel } from './components/admin-panel/admin-panel';
+
+//@Component({
+ // selector: 'app-root',
+ // imports: [AdminPanel],
+ // templateUrl: './app.html',
+ // styleUrl: './app.css'
+//})
+
+//import { AdminListaClientes } from './components/admin-lista-clientes/admin-lista-clientes';
+
+//@Component({
+  //selector: 'app-root',
+  //standalone: true,
+  //imports: [AdminListaClientes], // antes tenías AdminPanel aquí
+  //templateUrl: './app.html',
+  //styleUrl: './app.css'
+//})
+
+//import { AdminFormularioCliente } from './components/admin-formulario-cliente/admin-formulario-cliente';
+
+//@Component({
+  //selector: 'app-root',
+  //standalone: true,
+  //imports: [AdminFormularioCliente], // cambia esto
+  //templateUrl: './app.html',
+  //styleUrl: './app.css'
+//})
+
+
+import { AdminEscanerAcceso } from './components/admin-escaner-acceso/admin-escaner-acceso';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [AdminEscanerAcceso], // cambia esto
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {}
+export class App { }
+
